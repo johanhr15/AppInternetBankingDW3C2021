3443e868902efe9177971a6e0106412f1e7ba081
Bernardo Gonzalez <ber.gonzalezaraya@outlook.com>
2021-11-08 21:16:38 -0600

    Corrección  de proyecto

---
+++ AppWebInternetBanking/Models/SolTarjetaCreditoManager.cs
@@ -0,0 +1,66 @@
+﻿using AppWebInternetBanking.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppWebInternetBanking.Controllers
+{
+    public class SolTarjetaCreditoManager
+    {
+        string UrlBase = "http://localhost:49220/api/Sol_Tarjeta_Credito/";
+
+
+        private HttpClient GetClient(string token)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Add("Authorization", token);
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            return httpClient;
+        }
+
+
+        public async Task<Sol_Tarjeta_Credito> GetSolTarjetaCredito(string token, string codigo)
+        {
+            HttpClient httpClient = GetClient(token);
+            var response = await httpClient.GetStringAsync(string.Concat(UrlBase, codigo));
+            return JsonConvert.DeserializeObject<Sol_Tarjeta_Credito>(response);
+        }
+
+
+        public async Task<IEnumerable<Sol_Tarjeta_Credito>> GetSolTarjetasCredito(string token)
+        {
+            HttpClient httpClient = GetClient(token);
+            var response = await httpClient.GetStringAsync(UrlBase);
+            return JsonConvert.DeserializeObject<IEnumerable<Sol_Tarjeta_Credito>>(response);
+        }
+
+        public async Task<Sol_Tarjeta_Credito> Insert(Sol_Tarjeta_Credito sol_Tarjeta_Credito, string token)
+        {
+            HttpClient httpClient = GetClient(token);
+
+            var response = await httpClient.PostAsync(UrlBase,
+                new StringContent(JsonConvert.SerializeObject(sol_Tarjeta_Credito), Encoding.UTF8, "application/json"));
+
+            return JsonConvert.DeserializeObject<Sol_Tarjeta_Credito>(await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<Sol_Tarjeta_Credito> Update(Sol_Tarjeta_Credito sol_Tarjeta_Credito, string token)
+        {
+            HttpClient httpClient = GetClient(token);
+            var response = await httpClient.PutAsync(UrlBase,
+                new StringContent(JsonConvert.SerializeObject(sol_Tarjeta_Credito), Encoding.UTF8, "application/json"));
+            return JsonConvert.DeserializeObject<Sol_Tarjeta_Credito>(await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<int> Delete(int codigo, string token)
+        {
+            HttpClient httpClient = GetClient(token);
+            var response = await httpClient.DeleteAsync(string.Concat(UrlBase, codigo));
+            return JsonConvert.DeserializeObject<int>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
